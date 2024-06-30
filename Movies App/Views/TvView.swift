326c898c35f23b5b12760e8223334d8b079a4c07
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-06-30 13:44:54 +0300

    add TvView

---
+++ Movies App/Views/TvView.swift
@@ -0,0 +1,18 @@
+//
+//  TvView.swift
+//  Movies App
+//
+//  Created by Batuhan Berk Ertekin on 30.06.2024.
+//
+
+import SwiftUI
+
+struct TvView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TvView()
+}
